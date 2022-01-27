79fd62d785ef1ce832798aa0938833eefd5c07e4
stevendikowitz <stevendikowitz@gmail.com>
2022-01-27 15:45:54 -0500

    Allow booking multiple dates, fix for py3, new config file

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# reservator
+# reservator fork
 
+Forked from [howardbp/reservator](https://github.com/howardbp/reservator)
+
+## How to use
+
+1. git clone repo
+2. Update config-copy.py with your info
+3. rename config-copy.py to config.py
+4. `python3 res.py`
+
+# reservator
 
 the intention of this script is to create a reservation on a given date for a given restaurant (all entered in the config file) as soon as it becomes avaiable on Resy.
 
---
+++ config-copy.py
@@ -0,0 +1,17 @@
+USERNAME = "YOUR_EMAIL@gmail.com"
+PASSWORD = "YOUR_PASSWORD"
+
+
+# You can find the Resy venue_id for the restaurant you want using chrome inspector
+# on the restaurant page, fitler for Fetch/XHR, and reload. Filter "venue_id"
+VENUE_ID = "12345"
+GUESTS = 2
+
+# EXAMPLE DATES. ONLY PUT DATES IN THIS FORMAT
+DATES = [
+    "01/27/2022",
+    "01/28/2022",
+    "01/30/2022",
+]
+
+DATA = [USERNAME, PASSWORD, VENUE_ID, DATES, GUESTS]
